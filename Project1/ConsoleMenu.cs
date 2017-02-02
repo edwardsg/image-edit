eeea6d5a70646c3253e5a557936720a1e5f02dba
Gabriel <Gabriel@LAPTOP-EI97OEJ9>
2017-02-02 17:45:58 -0500

    Add project files.

---
+++ Project1/ConsoleMenu.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class ConsoleMenu
+    {
+		private ImageProcessor processor;
+		public ImageProcessor Processor
+		{
+			set
+			{
+				processor = value;
+			}
+		}
+
+        public void ShowMenu()
+        {
+			
+
+			string command = "";
+
+			while (!command.Equals("9"))
+			{
+				Console.WriteLine("\nImage Editor\n");
+
+				Console.WriteLine("1. Resize");
+				Console.WriteLine("2. Contrast");
+				Console.WriteLine("3. Brightness");
+				Console.WriteLine("4. Saturation");
+				Console.WriteLine("5. Blur");
+				Console.WriteLine("6. Sharpen");
+				Console.WriteLine("7. Edge Detection");
+				Console.WriteLine("8. Save File");
+				Console.WriteLine("9. Quit");
+
+				Console.Write("\nEnter command: ");
+
+				command = Console.ReadLine();
+
+				if (command.Equals("1"))
+				{
+					Console.Write("Enter new width: ");
+					int width = int.Parse(Console.ReadLine());
+					Console.Write("Enter new height: ");
+					int height = int.Parse(Console.ReadLine());
+
+					Console.WriteLine("Resize succeeded.");
+				}
+			}
+
+			processor.Exit();
+        }
+    }
+}
---
+++ Project1/ImageEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Project1
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class ImageEditor
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+			ConsoleMenu menu = new ConsoleMenu();
+			ImageProcessor processor = new ImageProcessor();
+
+			Console.Write("Enter file name: ");
+
+			string fileName = Console.ReadLine();
+			processor.FileName = fileName;
+
+			menu.Processor = processor;
+
+			Thread consoleThread = new Thread(new ThreadStart(menu.ShowMenu));
+			Thread processorThread = new Thread(new ThreadStart(processor.Run));
+
+			consoleThread.Start();
+			processorThread.Start();
+
+			consoleThread.Join();
+			processorThread.Join();
+
+			processor.Dispose();
+        }
+    }
+#endif
+}
